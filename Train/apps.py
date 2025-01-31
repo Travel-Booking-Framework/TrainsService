405baa272590180fb75a2283fe52c42d114a3fdb
Ali Ahmadi <aliahmadi79sh@gmail.com>
2025-01-31 10:16:26 +0330

    Add Signal for Sync Data in Elasticsearch + Configs

--- Train/apps.py
+++ Train/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class TrainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Train'
+
+    def ready(self):
+        # Import signals to ensure they're loaded
+        import Train.signals
